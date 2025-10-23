d2a21472966f125396f8bea94d7828ca5f1deee1
fareemomin <fareemomin7559@gmail.com>
2025-10-23 08:42:20 +0530

    Add files via upload

---
+++ MClass.java
@@ -0,0 +1,23 @@
+package implementation;
+import shapes.Circle;
+import shapes.Square;
+import numbers.OddEven;
+import numbers.PrimeorNot;
+public class MClass {
+
+	public static void main(String[] args) {
+		/*
+		shapes.Circle S1= new shapes.Circle();
+		shapes.Square S2= new shapes.Square();
+		numbers.OddEven N1=new numbers.OddEven();
+		
+		S1.CircleArea();
+		S2.SquareArea();
+		N1.OddEven();
+		*/
+		numbers.PrimeorNot N2=new numbers.PrimeorNot();
+		N2.checkPrime();
+		
+	}
+
+}
